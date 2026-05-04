1fcc90c8bba8388aa24e25146cca197942dcdd06
sameerdumne <dumnesameer@gmail.com>
2026-05-04 23:02:55 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,16 @@ import Navbar from '../components/Navbar'
 
 export const metadata = {
   title: 'Capify',
-  description: 'Discover music based on language, genre and mood.'
+  description: 'Discover music based on language, genre and mood.',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.png', sizes: '512x512', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ]
+  }
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
